49b9c67ac11bf4f179d1738d68f1cc13caa2acb6
Una865 <62244939+Una865@users.noreply.github.com>
2022-06-15 23:52:26 +0200

    Add files via upload

---
+++ zipper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import zipfile
+import os
+
+'''
+Usage:  ./zipper.py <extension>
+        ./zipper.py info 
+'''
+
+def zipper(folder, extension,name):
+
+
+
+    folder = os.path.abspath(folder)
+    zipf =  name + '.zip'
+    newZip = zipfile.ZipFile(zipf,'w')
+    for foldername, subfolder, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith(extension):
+                path = os.path.join(foldername,filename)
+                newZip.write(path, os.path.basename(path))
+                #newZip.write(filename)
+
+    newZip.close()
+
+import sys
+if len(sys.argv) < 2:
+    print("See info by running -> ./zipper.py info")
+elif sys.argv[1].lower() == 'info':
+    print("./zipper.py <folder_path> <extension> <zip file name>  - zip all files in the current folder with the specified extension")
+else:
+
+    folder = sys.argv[1]
+    extension = sys.argv[2]
+    zipf = sys.argv[3]
+
+    zipper(folder,extension,zipf)
+
+    print("Done")
+
+
+
